7dbc324624ad516494366a12b16d14f0b9753d06
metouole <yvesomda93@yahoo.fr>
2016-06-24 20:09:21 +0000

    add the option to destroy comment and implement dependences between post and comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,13 @@ class CommentsController < ApplicationController
 		redirect_to post_path(@post)
 	end
 
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comment.find(params[:id])
+		@comment.destroy
+
+		redirect_to post_path(@post)
+	end
 
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comment
+	has_many :comment, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5}
 	validates :body, presence: true
 end
