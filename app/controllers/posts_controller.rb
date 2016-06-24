d7b5463c5a27272b683da09cf84ba3f7b77559ec
metouole <yvesomda93@yahoo.fr>
2016-06-24 15:14:45 +0000

    Edit and Delete Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	
+
 
 	def index
 		@posts = Post.all.order('created_at DESC')
@@ -20,6 +22,26 @@ class PostsController < ApplicationController
 
 	def show 
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to root_path
 	end
 
 
